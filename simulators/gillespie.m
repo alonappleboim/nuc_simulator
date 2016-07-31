f6f70ce75f12b6ca07c2aa807eb8697e17ae9024
alonappleboim <alonappleboim@gmail.com>
2016-07-31 12:34:14 +0300

    changed gillespie to a function, added another script to use it, changed notation, added non zero-one nuc footprint

--- simulators/gillespie.m
+++ simulators/gillespie.m
@@ -1,134 +1,120 @@
-% SIMULATOR - Now With GILLESPIE For Extra Strength %
-% this file is split to cells with this "%%" - each can be executed
-% separately, which is sometimes very convinient...
+function [t,s_hist] = gillespie(model_params, varargin)
+%
+% gillespie simulation for nucleosome positioning 
+%
+% Arguments:
+%  modelparams - a struct with the following fields:
+%    nuc_w - width of a nucleosome
+%    slide_s - sliding step per unit time
+%    a - assembly rate per position along genome
+%    e - eviction rate per position along genome
+%    l - left slide rate per position along genome
+%    r - right slide rate per position along genome
+%    nuc_fp - nucleosome footprint for assembly between 0..1. 0
+%             means completely free, 1 means zero chance of assembly.
+%
+% Name/Value arguments:
+%    n_steps - number of simulation steps. default = genome size * 10.
+%    n_inst - number of instances to simulate. default = 10.
+%    s0 - state at t=0. default = random state.
+%    report - update every <report> itrations. default = n_steps/10. If
+%             'no', then no report is generated
+%  
 
-%% clear all?
-clear;
-clc;
-close all;
+% parse arguments
+mp = model_params;
+mp.genlen = length(mp.a);
+defaults = struct('n_steps', round(mp.genlen/mp.nuc_w*10),...
+                  'n_inst', 10,...
+                  's0', rand_state(mp.genlen, mp.nuc_w), ...
+                  'report', nan);
+sp = parse_namevalue_pairs(defaults, varargin); %simulation parameters
+if isnan(sp.report)
+    sp.to_report = true;
+    sp.report = sp.n_steps./10;
+else
+    if strcmp(sp.to_report, 'no'), sp.to_report = false;
+    else sp.to_report = true; end
+end
 
+% prep simulation
+slide_k = ones(1, mp.nuc_w+mp.slide_s); % the kernel for the sliding test
+rand_nums = rand(1, sp.n_steps);  % random numbers for calculating the dt of each change (GILLESPIE)
 
-%% %%% PARAMETERS %%%%%
+s_hist = [sp.s0; zeros(sp.n_steps, mp.genlen)]; % the state history
+t = zeros(sp.n_steps, 1); % keeping track of simulation time
 
-REP_EVERY = 1000;
-gen_len = 4000;                % the length of the genome
-nuc_len = 147;                 % the length of a single nucleosome (odd number for convenience)
-jump_len = 20; 				   % the length of a single jump (left or right) - even for convenience
-nuc_vec = ones(1,(nuc_len*2)-1); % the vector for the in_vec convulution
-jump_vec = ones(1,nuc_len+jump_len); % the vector for the jumping convulutions
-a_rate = ones(1,gen_len);     % a rate vector for the assembly of a nucleosome into each bp
-e_rate = ones(1,gen_len);     % a rate vector for the evicition of a nucleosome from each bp
-r_rate = .1.*ones(1,gen_len);     % a rate vector for the right slide from each bp
-l_rate = zeros(1,gen_len);     % a rate vector for the left slide from each bp
-num_steps = 5000;              % the number of steps to simulate
-rand_nums = rand(1,num_steps);  % random numbers for calculating the dt of each change (GILLESPIE)
-state = zeros(1,gen_len);     % the initial state
-
-state_hist = zeros(num_steps+1,gen_len); % the state history
-time = zeros(num_steps,1); % keeping track of simulation time
-state_hist(1,:) = state; 
-
-% change the input probabilities so that we have an NFR in the middle:
-nfr_hw = 200;
-a_rate((gen_len/2-nfr_hw):(gen_len/2)+nfr_hw) = 0;
-
-%% %%% MAIN LOOP %%%%%
-
-for i=1:num_steps
-    
+% time loop
+state = sp.s0;
+for i = 1:sp.n_steps
     % report progress:
-    if(mod(i,REP_EVERY)==0)
+    if sp.to_report && mod(i,sp.report) == 0
         clc;
-        fprintf('At iteration %i (%d%%)...\n', i, 100*i./num_steps);
+        fprintf('At iteration %i (%d%%)...\n', i, 100*i./sp.n_steps);
     end;
     
-	% generate the assembly, eviction and slide vectors:
-    evic = state; % which bp can have a nuc evicted from them
+	% generate the assembly, eviction and sliding rate vectors
+    er = state .* mp.e; % eviction rate
+    free_dna = (1-min(1,conv(state, mp.nuc_fp, 'same')));
+	ar = free_dna.*mp.a; %assembly rate
+	lr = zeros(1,mp.genlen);
+    rr = zeros(1,mp.genlen);
+% 	can_slide = conv(state, slide_k, 'same');
+% 	temp_jump_vec(temp_jump_vec ~= 0) = 1;
+%     temp_jump_vec = ~temp_jump_vec; % ones are the positions that indicate that there is enough space for a jump
 	
-	assem = conv(state,nuc_vec,'same'); % which bp can have a nuc inserted to them
-	assem(assem~=0) = 1;
-	assem = ~assem;
-	
-	temp_jump_vec = conv(state,jump_vec,'same');
-	temp_jump_vec(temp_jump_vec ~= 0) = 1;
-    temp_jump_vec = ~temp_jump_vec; % ones are the positions that indicate that there is enough space for a jump
-	
-	temp_right_vec = find(temp_jump_vec) - (jump_len/2) - (2*fix(nuc_len/2)); % find the bps that can jump right
-	temp_left_vec = find(temp_jump_vec) + (jump_len/2) + (2*fix(nuc_len/2)); % find the bps that can jump left
-	
-	right_vec = zeros(1,gen_len); % which bp have a nuc that can jump right
-    temp = temp_right_vec > 0;
-	right_vec(temp_right_vec(temp)) = 1; % remove bps that are left of the edge (can't jump right)
-	left_vec = zeros(1,gen_len); % which bp have a nuc that can jump left
-    temp = temp_left_vec <= gen_len;
-	left_vec(temp_left_vec(temp)) = 1; % remove bps that are right of the edge (can't jump left)
+% 	temp_right_vec = find(temp_jump_vec) - (jump_len/2) - (2*fix(nuc_len/2)); % find the bps that can jump right
+% 	temp_left_vec = find(temp_jump_vec) + (jump_len/2) + (2*fix(nuc_len/2)); % find the bps that can jump left
+% 	
+% 	right_vec = zeros(1,gen_len); % which bp have a nuc that can jump right
+%     temp = temp_right_vec > 0;
+% 	right_vec(temp_right_vec(temp)) = 1; % remove bps that are left of the edge (can't jump right)
+% 	left_vec = zeros(1,gen_len); % which bp have a nuc that can jump left
+%     temp = temp_left_vec <= gen_len;
+% 	left_vec(temp_left_vec(temp)) = 1; % remove bps that are right of the edge (can't jump left)
+
+    % sample next time point
+    all_rates = er + ar + lr + rr;
+    dt = -log(rand_nums(i))./sum(all_rates);
     
-    % make sure the edges are NFRs:
-    %{
-    for j=1:(fix(nuc_len/2))
-        in_vec(j)=0;
-        in_vec(end-j+1)=0;
+    % sample a position, and a state change:
+	p = [er,ar,lr,rr];
+    nzidx = find(p~=0);
+    p = p(p~=0)./sum(p);
+	ind = nzidx(mnrnd(1,p)==1);
+    pos = mod(ind, mp.genlen);
+    step = (ind-pos)./mp.genlen + 1;
+	if pos == 0
+        pos = mp.genlen;
+        step = step - 1;
     end
-    %}
     
-	% generate the change_matrix:
-	ep = evic .* e_rate; % probability of eviction
-	ap = assem .* a_rate;  % .. of assembly
-	lp = left_vec .* state .* l_rate; % .. sliding left 
-	rp = right_vec .* state .* r_rate; % .. and sliding right
-	p = ep + ap + lp + rp;
-    sp = sum(p);
-    p = p./sp;
-	
-	% find how much time passed until the next change (simulating exponential distribution):
-	dt = -log(rand_nums(i))./sp;
-	
-	% sample a bp for a change, and sample a state change:
-	bp = find(mnrnd(1,p));
-	change = find(mnrnd(1,[ep(bp),ap(bp),lp(bp),rp(bp)]));
-	
 	% make the change in the state:
-	if (change == 2)
-		state(bp) = 1;
-    else
-        state(bp) = 0;
-        if (change == 3)
-            state(bp-jump_len) = 1;
+	if (step == 2) %assembly
+		state(pos) = 1;
+    else %eviction
+        state(pos) = 0;
+        if (step == 3) %left slide
+            state(pos-jump_len) = 1;
         end
-        if (change == 4)
-            state(bp+jump_len) = 1;
+        if (step == 4) %right slide
+            state(pos+jump_len) = 1;
         end
 	end
-	state_hist(i+1,:) = state; % we multiply by dt to get the duration of the state
-    time(i+1) = time(i) + dt;
+	s_hist(i+1,:) = state;
+    t(i+1) = t(i) + dt;
 end
-clc;
 
-%% %%% FINAL CALCULATIONS %%%%%
+if sp.to_report, clc; end;
+end
 
-% get the number of times each bp had a nuceosome center on it
-centers_vector = sum(state_hist(:,:));
-smooth_vector =  conv(centers_vector, ones(1,nuc_len), 'same');
-
-% make the two graphs be of the same order of magnitude:
-smooth_vector_smaller = smooth_vector .* (max(centers_vector)/mean(smooth_vector));
-
-%% %%% OUTPUT GRAPHS %%%%%
-
-%{
-figure;
-plot(centers_vector)
-title('Number of Nucleosome Centers VS Base Pair')
-figure;
-plot(smooth_vector)
-title('Number of Nucleosome Coverage VS Base Pair')
-%}
-figure;
-plot(centers_vector,'r')
-hold on
-plot(smooth_vector_smaller,'g')
-legend('centers','smooth')
-title('Combined')
-xlabel('Position')
-ylabel('Time')
-hold off
+function s = rand_state(N,w)
+    s = rand(N,1) < 1./(2.*w+1);
+    while true
+        idx = find(s);
+        r = find(diff(idx) < 2*w+1);
+        if isempty(r), break; end;
+        s(idx(r)) = 0;
+    end
+    s = 1.*s.';
+end
---
+++ simulators/simulations.m
@@ -0,0 +1,144 @@
+
+%% %%% PARAMETERS %%%%%
+
+mp = struct();
+GL = 5000;
+mp.a = ones(1,GL); mp.a(2400:2800) = .1;
+mp.e = ones(1,GL); mp.e(2600:300) = 50;
+mp.r = zeros(1,GL);
+mp.l = zeros(1,GL);
+mp.nuc_w = 75;
+mp.slide_s = 10;
+x = -1:(10./(mp.nuc_w-2)):9;
+mp.nuc_fp = [1./(1+exp(-1.*x))];
+mp.nuc_fp = [mp.nuc_fp, 1, fliplr(mp.nuc_fp)];
+%plot the footprint
+clf;
+hold all;
+for i = 0:2:8, plot(mp.nuc_fp>(1-10^-i),'linewidth',2); end
+legend(strcat('fp > 1-(10^',num2str([0:-2:-8]'),')'), 'location','south')
+plot(mp.nuc_fp, 'linewidth',2);
+%%
+
+[t, s_hist] = gillespie(mp, 'n_steps', 1e4);
+
+%%
+
+REP_EVERY = 1000;
+gen_len = 4000;                % the length of the genome
+nuc_len = 147;                 % the length of a single nucleosome (odd number for convenience)
+jump_len = 20; 				   % the length of a single jump (left or right) - even for convenience
+nuc_vec = ones(1,(nuc_len*2)-1); % the vector for the in_vec convolution
+jump_vec = ones(1,nuc_len+jump_len); % the vector for the jumping convolutions
+num_steps = 5000;              % the number of steps to simulate
+rand_nums = rand(1,num_steps);  % random numbers for calculating the dt of each change (GILLESPIE)
+state = zeros(1,gen_len);     % the initial state
+
+state_hist = zeros(num_steps+1,gen_len); % the state history
+time = zeros(num_steps,1); % keeping track of simulation time
+state_hist(1,:) = state; 
+
+% change the input probabilities so that we have an NFR in the middle:
+nfr_hw = 200;
+a_rate((gen_len/2-nfr_hw):(gen_len/2)+nfr_hw) = 0;
+
+%% %%% MAIN LOOP %%%%%
+
+for i=1:num_steps
+    
+    % report progress:
+    if(mod(i,REP_EVERY)==0)
+        clc;
+        fprintf('At iteration %i (%d%%)...\n', i, 100*i./num_steps);
+    end;
+    
+	% generate the assembly, eviction and slide vectors:
+    evic = state; % which bp can have a nuc evicted from them
+	
+	assem = conv(state,nuc_vec,'same'); % which bp can have a nuc inserted to them
+	assem(assem~=0) = 1;
+	assem = ~assem;
+	
+	temp_jump_vec = conv(state,jump_vec,'same');
+	temp_jump_vec(temp_jump_vec ~= 0) = 1;
+    temp_jump_vec = ~temp_jump_vec; % ones are the positions that indicate that there is enough space for a jump
+	
+	temp_right_vec = find(temp_jump_vec) - (jump_len/2) - (2*fix(nuc_len/2)); % find the bps that can jump right
+	temp_left_vec = find(temp_jump_vec) + (jump_len/2) + (2*fix(nuc_len/2)); % find the bps that can jump left
+	
+	right_vec = zeros(1,gen_len); % which bp have a nuc that can jump right
+    temp = temp_right_vec > 0;
+	right_vec(temp_right_vec(temp)) = 1; % remove bps that are left of the edge (can't jump right)
+	left_vec = zeros(1,gen_len); % which bp have a nuc that can jump left
+    temp = temp_left_vec <= gen_len;
+	left_vec(temp_left_vec(temp)) = 1; % remove bps that are right of the edge (can't jump left)
+    
+    % make sure the edges are NFRs:
+    %{
+    for j=1:(fix(nuc_len/2))
+        in_vec(j)=0;
+        in_vec(end-j+1)=0;
+    end
+    %}
+    
+	% generate the change_matrix:
+	ep = evic .* e_rate; % probability of eviction
+	ap = assem .* a_rate;  % .. of assembly
+	lp = left_vec .* state .* l_rate; % .. sliding left 
+	rp = right_vec .* state .* r_rate; % .. and sliding right
+	p = ep + ap + lp + rp;
+    sp = sum(p);
+    p = p./sp;
+	
+	% find how much time passed until the next change (simulating exponential distribution):
+	dt = -log(rand_nums(i))./sp;
+	
+	% sample a bp for a change, and sample a state change:
+	bp = find(mnrnd(1,p));
+	change = find(mnrnd(1,[ep(bp),ap(bp),lp(bp),rp(bp)]));
+	
+	% make the change in the state:
+	if (change == 2)
+		state(bp) = 1;
+    else
+        state(bp) = 0;
+        if (change == 3)
+            state(bp-jump_len) = 1;
+        end
+        if (change == 4)
+            state(bp+jump_len) = 1;
+        end
+	end
+	state_hist(i+1,:) = state;
+    time(i+1) = time(i) + dt;
+end
+clc;
+
+%% %%% FINAL CALCULATIONS %%%%%
+
+% get the number of times each bp had a nuceosome center on it
+centers_vector = sum(state_hist(:,:));
+smooth_vector =  conv(centers_vector, ones(1,nuc_len), 'same');
+
+% rescale graphs
+smooth_vector = smooth_vector .* (max(centers_vector)/mean(smooth_vector));
+
+%% %%% OUTPUT GRAPHS %%%%%
+
+%{
+figure;
+plot(centers_vector)
+title('Number of Nucleosome Centers VS Base Pair')
+figure;
+plot(smooth_vector)
+title('Number of Nucleosome Coverage VS Base Pair')
+%}
+figure(1);
+plot(centers_vector,'r')
+hold on
+plot(smooth_vector_smaller,'g')
+legend('centers','coverage')
+title('Combined')
+xlabel('Position')
+ylabel('Time')
+hold off
