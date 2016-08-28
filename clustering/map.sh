28e765735516780d04dba894ebff4fd9d47e5975
Daniel Gissin <Daniel Gissin>
2016-08-28 15:50:00 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/map.sh
+++ clustering/map.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/experiment_data/wt_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); [nuc_sum,feature]=cluster_sim($1,1001,sequences_structure,wt_3h); quit;"
+matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/experiment_data/wt_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); [nuc_sum,feature]=cluster_sim($1,1001,sequences_structure,wt_3h); quit;" &
+
+wait
