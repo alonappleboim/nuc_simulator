f94ad0249b8df25becbeeb08b84480685c240156
Daniel Gissin <Daniel Gissin>
2016-08-30 11:04:50 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/Review_Simulation.m
+++ clustering/Review_Simulation.m
@@ -2,7 +2,7 @@ load('C:\Users\Daniel\Documents\MATLAB\Friedman Lab\Experiment Data\wt_centers.m
 load('C:\Users\Daniel\Documents\MATLAB\Friedman Lab\Experiment Data\sequences_structure.mat')
 
 NFR_pos = [701:1200];
-gene_id = 5555;
+gene_id = 6641;
 
 create_full_params;
 
--- clustering/map.sh
+++ clustering/map.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/experiment_data/wt_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); [nuc_sum,feature]=cluster_sim($1,5555,sequences_structure,wt_3h); quit;" &
+matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/experiment_data/wt_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); [nuc_sum,feature]=cluster_sim($1,6641,sequences_structure,wt_3h); quit;" &
 
 wait
 
