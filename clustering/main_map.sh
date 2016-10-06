fce8754773ecf4f077cab176fd4dad3ca7d02de4
Daniel Gissin <Daniel Gissin>
2016-10-06 11:13:46 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/main_map.sh
+++ clustering/main_map.sh
@@ -3,7 +3,7 @@
 echo $1
 echo $2
 
-for i in {1.."$2"}
+for (i=1 ; i<=$2 ; i++);
 do
 	sbatch -D /cs/bd/Daniel/simulations/logs -e log --mem 2000 "/cs/bd/Daniel/nuc_simulator/clustering/map.sh" $i $1
 done
