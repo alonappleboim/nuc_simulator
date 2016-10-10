42360dd865774d2ddf961874eeda7a76b6e6d2e0
Daniel Gissin <Daniel Gissin>
2016-10-10 12:34:51 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- simulators/get_NFR_features_data.m
+++ simulators/get_NFR_features_data.m
@@ -8,21 +8,31 @@ function [ plus_one, minus_one, NFR_width ] = get_NFR_features_data( centers )
 smoothed = ksdensity([1:length(centers)],[1:length(centers)],'weights',double(centers),'width',20);
 [peaks,positions] = findpeaks(smoothed, 'MinPeakHeight', 2*10^(-4));
 
-% get the top 10 peaks so we lose the small ones:
-[temp,indices] = sort(peaks,'descend');
-indices = sort(indices);
-indices = indices(1:10);
-positions = positions(indices);
+if (length(peaks) > 9)
+    
+    % get the top 10 peaks so we lose the small ones:
+    [temp,indices] = sort(peaks,'descend');
+    indices = sort(indices);
+    indices = indices(1:10);
+    positions = positions(indices);
 
-% find the +1 position:
-temp = positions(positions > 949);
-plus_one = temp(1);
+    % find the +1 position:
+    temp = positions(positions > 949);
+    plus_one = temp(1);
 
-% find the -1 position:
-temp = positions(positions < 950);
-minus_one = temp(end);
+    % find the -1 position:
+    temp = positions(positions < 950);
+    minus_one = temp(end);
 
-NFR_width = plus_one - minus_one;
+    NFR_width = plus_one - minus_one;
+   
+else
+    
+    plus_one = 0;
+    minus_one = 0;
+    NFR_width = 0;
+    
+end
 
 end
 
