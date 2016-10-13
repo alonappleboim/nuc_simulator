4339400d719af51a01d3882cc156241972a09c8d
Daniel Gissin <Daniel Gissin>
2016-10-13 16:04:30 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/gene_map.sh
+++ clustering/gene_map.sh
@@ -6,7 +6,7 @@ do
 	for ((i=0 ; i<=9 ; i++));
 	do
 		let "k=$j*10+$i+1"
-		matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/nuc_simulator/clustering/experiment_data/sth1_6h_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); full_cluster_sim($k,$1,sequences_structure,wt_3h); quit;" &
+		matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/nuc_simulator/clustering/experiment_data/sth1_6h_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); full_cluster_sim($k,$1,sequences_structure,sth1_6h); quit;" &
 	done
 
 	wait
