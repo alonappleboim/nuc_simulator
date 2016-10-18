ff6aaa1f96b6a553fef1926481e74a5263438752
Daniel Gissin <Daniel Gissin>
2016-10-18 10:35:42 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/full_cluster_reduce.m
+++ clustering/full_cluster_reduce.m
@@ -15,7 +15,7 @@ nuc_sums = zeros(num_of_runs, 3500);
 
 % load all of the results:
 for i = 1:num_of_runs
-	load(['/cs/bd/Daniel/simulations/output/sim_' num2str(i) 'gene_' num2str(gene_index) '.mat']);
+	load(['/cs/bd/Daniel/simulations/full_output/sim_' num2str(i) 'gene_' num2str(gene_index) '.mat']);
     
     if (length(nuc_sum) == 1) % in case the gene was a NaN gene...
         return
