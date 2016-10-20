ca33ab71e9086985b0c71ba0e1e125ecdd568f39
Daniel Gissin <Daniel Gissin>
2016-10-20 11:01:12 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/full_genome_map.sh
+++ clustering/full_genome_map.sh
@@ -2,5 +2,5 @@
 
 for ((i=1 ; i<=153 ; i++));
 do
-	sbatch -D /cs/bd/Daniel/simulations/logs -e log --mem 10000 "/cs/bd/Daniel/nuc_simulator/clustering/gene_map.sh" $i
+	sbatch -D /cs/bd/Daniel/simulations/logs -e log --mem 10001 "/cs/bd/Daniel/nuc_simulator/clustering/gene_map.sh" $i
 done
