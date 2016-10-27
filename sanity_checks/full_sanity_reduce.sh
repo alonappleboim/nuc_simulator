2c7ab306a6a8f9d39e1e92b973691c36313d2095
Daniel Gissin <Daniel Gissin>
2016-10-27 10:08:42 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

---
+++ sanity_checks/full_sanity_reduce.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for ((i=1 ; i<=153 ; i++));
+do
+	sbatch -D /cs/bd/Daniel/simulations/logs -e log --mem 10001 "/cs/bd/Daniel/nuc_simulator/sanity_checks/sanity_reduce.sh" $i
+done
+
+wait
---
+++ sanity_checks/sanity_reduce.m
@@ -0,0 +1,31 @@
+function sanity_reduce(gene_index)
+
+% load all the necessary data:
+load('/cs/bd/Daniel/nuc_simulator/clustering/experiment_data/wt_centers.mat');
+load('/cs/bd/Daniel/experiment_data/sequences_structure.mat');
+addpath(genpath('/cs/bd/Daniel/nflab_scripts'));
+addpath(genpath('/cs/bd/Daniel/nuc_simulator'));
+
+features = zeros(10,10);
+
+% load all of the results:
+for i = 1:10
+    for j = 1:10
+        try
+            load(['/cs/bd/Daniel/simulations/sanity_checks/sim_' num2str(i) '_' num2str(j) 'gene_' num2str(gene_index) '.mat']);
+        catch a
+            features(i,j) = -100000;
+            continue
+        end
+
+        if (length(nuc_sum) == 1) % in case the gene was a NaN gene...
+            return
+        end
+
+        features(i,j) = likelihood;
+    end
+end
+
+% save to a new .mat file:
+save(['/cs/bd/Daniel/simulations/sanity_checks/sanity_results_' num2str(gene_index)] , ...
+	'features');
