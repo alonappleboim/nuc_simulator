ab9e8519c461d5bf4ac3b735900de969d4995230
Daniel Gissin <Daniel Gissin>
2016-10-27 10:22:30 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

---
+++ sanity_checks/review_sanity.m
@@ -0,0 +1,17 @@
+load('C:\Users\Daniel\Documents\MATLAB\Friedman Lab\Experiment Data\wt_centers.mat')
+load('C:\Users\Daniel\Documents\MATLAB\Friedman Lab\Experiment Data\sequences_structure.mat')
+
+for i = 1:10
+
+    try
+        load(['C:\Users\Daniel\Documents\MATLAB\nuc_simulator\sanity_checks\results\sanity_results_' num2str(gene_index)])
+    catch a
+        continue
+    end
+
+    contour(features)
+    xlabel('RSC Length Parameter')
+    ylabel('RSC Eviction Intensity Parameter')
+    legend('Likelihood Test Result')
+    title(['Likelihood Result as a function of RSC Length and Eviction Intensity - gene ' num2str(i)])
+end
