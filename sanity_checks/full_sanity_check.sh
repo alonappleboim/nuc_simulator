f139c761e8c1b57c10000e9b61b2b0441debdfd6
Daniel Gissin <Daniel Gissin>
2016-10-27 15:57:49 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- sanity_checks/full_sanity_check.sh
+++ sanity_checks/full_sanity_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((i=1 ; i<=153 ; i++));
+for ((i=1 ; i<=80 ; i++));
 do
 	sbatch -D /cs/bd/Daniel/simulations/logs -e log --mem 10001 "/cs/bd/Daniel/nuc_simulator/sanity_checks/sanity_check.sh" $i
 done
