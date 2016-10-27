352b10d492349b1cca9e1d24e735f6e05ab55bbf
Daniel Gissin <Daniel Gissin>
2016-10-27 17:05:42 +0300

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- sanity_checks/sanity_check.sh
+++ sanity_checks/sanity_check.sh
@@ -5,7 +5,7 @@ do
 
 	for ((j=1 ; j<=10 ; j++));
 	do
-		matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/nuc_simulator/clustering/experiment_data/wt_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); sanity_check($1,sequences_structure,wt_3h,$i,$j); quit;" &
+		matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('/cs/bd/Daniel/nuc_simulator/clustering/experiment_data/wt_centers.mat'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); sanity_check($1,sequences_structure,wt_3h,$i,$j); quit;"  &
 	done
 
 	wait
