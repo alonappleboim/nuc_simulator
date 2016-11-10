07015c3453697df1b00076fab4cd0629a9c7dbf8
Daniel Gissin <Daniel Gissin>
2016-11-10 13:37:28 +0200

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/create_full_params_RSC_ratio.m
+++ clustering/create_full_params_RSC_ratio.m
@@ -3,6 +3,6 @@ tf_rates = [0.0001]; % this is the TF assembly rate, and the eviction will be ha
 tf_evic_eff = [0.0001];
 rsc_length = [20, 40, 45, 50, 65, 75]; % this is the eviction length of RSC - the sliding length will be twice this
 rsc_evic_eff = [0.02, 0.04, 0.07, 0.1, 0.2];
-rsc_slide_eff = [10, 20, 40, 70, 100]; % the ratio between slide and eviction
+rsc_slide_eff = [2, 5, 10, 15, 20, 40, 70, 100]; % the ratio between slide and eviction
 
 params = combvec(poly_rates, tf_rates, tf_evic_eff, rsc_length, rsc_evic_eff, rsc_slide_eff);
