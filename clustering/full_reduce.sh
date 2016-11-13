e5e05a22d0886c74db303944c59e2c415f1309bc
Daniel Gissin <Daniel Gissin>
2016-11-13 09:03:24 +0200

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/full_reduce.sh
+++ clustering/full_reduce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-matlab -nodesktop -nosplash -nodisplay -nojvm -r "addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); full_cluster_reduce($1);  quit;"
+matlab -nodesktop -nosplash -nodisplay -nojvm -r "addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); full_cluster_reduce_2($1);  quit;"
