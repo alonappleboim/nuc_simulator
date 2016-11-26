dccf512cfe80441cd52411935a86364d2941cbd2
Daniel Gissin <Daniel Gissin>
2016-11-26 10:16:55 +0200

    Signed-off-by: Daniel Gissin <Daniel Gissin>

--- clustering/temp_gene_reduce.sh
+++ clustering/temp_gene_reduce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('$3'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); cluster_reduce($1,data,'$4');  quit;"
+matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('$3'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); cluster_reduce_global_search($1,data,'$4');  quit;"
