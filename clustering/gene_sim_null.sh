fea52882709a30346e69fad00692b5627aa55b4a
Daniel <dsgissin@gmail.com>
2017-02-01 16:01:54 +0200

    Signed-off-by: Daniel <dsgissin@gmail.com>

--- clustering/gene_sim_null.sh
+++ clustering/gene_sim_null.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('$2'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); cluster_null($1,sequences_structure,data,'$3'); quit;" &
+matlab -nodesktop -nosplash -nodisplay -nojvm -r "load('$2'); load('/cs/bd/Daniel/experiment_data/sequences_structure.mat'); addpath(genpath('/cs/bd/Daniel/nflab_scripts')); addpath(genpath('/cs/bd/Daniel/nuc_simulator')); cluster_null($1,sequences_structure,data,'$3'); quit;"
